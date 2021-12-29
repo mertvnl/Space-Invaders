5180d35d0f8938282b5f20015fc31ea842e1a6de
mertvnl <41839620+mertvnl@users.noreply.github.com>
2021-12-29 20:11:18 +0300

    setup: basic scene setup with background controller

---
+++ Assets/SpaceInvaders/Scripts/Controllers/BackgroundController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    #region Getters
+    private Material material;
+    public Material Material { get { return material == null ? material = GetComponent<Renderer>().material : material; } }
+    #endregion
+
+    [SerializeField] private float backgroundSpeed;
+
+    private Vector2 backgroundMovementAxis;
+
+    private void Awake()
+    {
+        SetRandomDirection();    
+    }
+
+    private void SetRandomDirection()
+    {
+        backgroundMovementAxis = new Vector2(Random.Range(-1f, 1f), 1);
+    }
+
+    private void Update()
+    {
+        if (Material == null)
+            return;
+
+        Material.mainTextureOffset += backgroundMovementAxis * backgroundSpeed * Time.deltaTime;
+    }
+}
