c3e89e7c6906e5ec55b88d2d7ce75c1959044a83
mertvnl <41839620+mertvnl@users.noreply.github.com>
2021-12-30 03:20:56 +0300

    bug fix: missile rotation bug fix, prefab update

--- Assets/SpaceInvaders/Scripts/Controllers/MissileControllers/MissileControllerBase.cs
+++ Assets/SpaceInvaders/Scripts/Controllers/MissileControllers/MissileControllerBase.cs
@@ -5,16 +5,14 @@ using UnityEngine;
 public abstract class MissileControllerBase : MonoBehaviour
 {
     [SerializeField] private Transform firePoint;
+    [SerializeField] private string defaultMissileID;
+
     public float FireRate;
     public bool CanFire = true;
 
-    public GameObject defaultMissile;
-
-
     public virtual void FireMissile()
     {
-        //IMissile missile = Instantiate(defaultMissile, firePoint.position, firePoint.rotation).GetComponent<IMissile>();
-        IMissile missile = PoolingManager.Instance.InstantiatePoolObject("DefaultMissileBlue", transform.position, Quaternion.identity).GetComponent<IMissile>();
+        IMissile missile = PoolingManager.Instance.InstantiatePoolObject(defaultMissileID, transform.position, transform.rotation).GetComponent<IMissile>();
         missile.Initialise(gameObject.layer);
     }
 }
